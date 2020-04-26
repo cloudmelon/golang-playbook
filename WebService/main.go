dad287d8e035e8ac0aa865f189531b369b29431e
cloudmelon <urinaway@gmail.com>
2020-04-26 22:32:05 +0100

    web service

--- WebService/main.go
+++ WebService/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"cloudmelon/webservice/models"
+	"fmt"
+)
 
+func main() {
+	u := models.User{
+		ID:        2,
+		FirstName: "Lucie",
+		LastName:  "Cat",
+	}
+	fmt.Println(u)
 }
